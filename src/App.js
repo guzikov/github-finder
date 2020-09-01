7a9d7dfe7454fcc9e5493858b7182e34069c8014
Sergei Guzikov <sguzikov@gmail.com>
2020-09-01 14:09:07 +0300

    repos component

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     user: {},
     loading: false,
     alert: null,
+    repos: [],
   };
 
   searchUsers = async (text) => {
@@ -35,6 +36,15 @@ class App extends Component {
     );
 
     this.setState({ user: res.data, loading: false });
+  };
+
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
   };
 
   clearUsers = () => this.setState({ users: [], loading: false });
@@ -78,7 +88,10 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={this.state.user}
+                    repos={this.state.repos}
+                    loading={this.state.loading}
                   />
                 )}
               />
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,14 +1,17 @@
 import React, { Fragment, Component } from 'react';
 import Sprinner from '../layout/Spinner';
 import { Link } from 'react-router-dom';
+import Repos from '../repos/Repos';
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
   render() {
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
@@ -22,13 +25,58 @@ export class User extends Component {
       hireable,
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Sprinner />;
 
     return (
       <Fragment>
-        <Link to='/'>Back to Search</Link>
+        <Link to='/' className='btn btn-light'>
+          Back to Search
+        </Link>
+        Hireable: {''}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='grid-2 card'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              alt=''
+              className='round-img'
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit Github Account
+            </a>
+            <ul>
+              <li>{login && <Fragment>Username: {login}</Fragment>}</li>
+              <li>{company && <Fragment>Company: {company}</Fragment>}</li>
+              <li>{blog && <Fragment>Website: {blog}</Fragment>}</li>
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-danger'>Following: {following}</div>
+          <div className='badge badge-light'>
+            Public Reposetories: {public_repos}
+          </div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
